ee5ccb606de0a45c98e3926670837435f2ae8570
seb <sgiles@student.ethz.ch>
2020-03-22 19:22:01 +0100

    removed build directory from workspace

--- learn2soar/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/evalli/catkin_ws/src/rotors_simulator/learn2soar
-# Build directory: /home/evalli/catkin_ws/src/rotors_simulator/learn2soar/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
