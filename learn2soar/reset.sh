de10ca1034b12121889d5c6b7ef1bf800d17c66b
seb <sgiles@student.ethz.ch>
2020-03-30 23:30:32 +0200

    tracking a tilted triangle with limited prop

--- learn2soar/reset.sh
+++ learn2soar/reset.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState "{model_name: 'uav_1', pose: {position: {x: 10, y: -50, z: 45}, orientation: {x: 0, y: 0, z: 0, w: 1}}, twist: {linear: {x: 25, y: 0, z: 0}, angular: {x: 0, y: 0, z: 0}}, reference_frame: ground_collision_plane}"
+rostopic pub -1 /gazebo/set_model_state gazebo_msgs/ModelState "{model_name: 'uav_1', pose: {position: {x: 0, y: -100, z: 50}, orientation: {x: 0, y: 0, z: 0, w: 1}}, twist: {linear: {x: 20, y: 0, z: 0}, angular: {x: 0, y: 0, z: 0}}, reference_frame: ground_collision_plane}"
--- learn2soar/scripts/learn2soar.py
+++ learn2soar/scripts/learn2soar.py
@@ -15,6 +15,8 @@ class Learn2Soar:
         self.roll_pitch_control_period = 0.01
         self.alt_control_period = 0.1
         
+        self.last_phi_err = 0
+
         self.alt = None
         self.alt_ref = 50
         self.last_alt_err = 0
@@ -23,27 +25,32 @@ class Learn2Soar:
         
         self.last_wp_id = 0
         self.psi_integrator = 0       
-        self.last_psi_err = 0
+        self.last_psi = 0
         self.phi_ref = 0
 
-        self.prop = None
+        self.last_curv = 0
+
+        self.prop = 0
         self.elev = None
         self.ail = None
 
         self.theta = None
         self.phi = None
 
+        self.airSpeed = None
+
 
     def update_sensor_data(self, pose, twist):
         self.pose = pose
         self.twist = twist
+
 
         
     def do_alt_control(self):
         
         ## waypoint navigation
 
-        wp_int = np.pi / 2 # waypoint interval
+        wp_int = np.pi*2./3. # waypoint interval
         traj_radius = 100
 
         x = self.pose.position.x
@@ -54,12 +61,10 @@ class Learn2Soar:
         wp_id = np.floor(ang_position/wp_int) + 1
         wp_ang_position = wp_int*wp_id
         wp_x, wp_y = traj_radius*np.array([np.cos(wp_ang_position), np.sin(wp_ang_position)])
+        d = np.linalg.norm([wp_x-x, wp_y-y])
+        wp_dir = np.arctan2(wp_y-y, wp_x-x)
 
-        ## altitude control
-        if self.pose.position.x <= 100:
-            self.alt_ref = 50
-        else:
-            self.alt_ref = 50
+        self.alt_ref = 40. + 15. * x / 50.
 
         alt = self.pose.position.z
         alt_err = (self.alt_ref - alt)
@@ -68,7 +73,7 @@ class Learn2Soar:
         K_i_alt = 0.05
         K_d_alt = 0.0050
 
-        theta_limit = 0.3*np.pi/2
+        theta_limit = 0.5*np.pi/2
 
         self.alt_integrator += alt_err * self.alt_control_period
         if K_i_alt > 0:
@@ -84,43 +89,46 @@ class Learn2Soar:
 
         ## heading control
 
-        psi_ref = np.arctan2(wp_y-y, wp_x-x)
-
         quat = (self.pose.orientation.x,
                 self.pose.orientation.y,
                 self.pose.orientation.z,
                 self.pose.orientation.w)
 
         euler = tf.transformations.euler_from_quaternion(quat)
-        psi     = euler[2]  # heading
 
-        psi_err = np.mod((psi_ref - psi) + np.pi , 2*np.pi) - np.pi
+        psi = euler[2]  # heading
 
-        K_p_head = 1.5   #0.01
-        K_i_head = 0.3   #0.05
-        K_d_head = 0.2    #0.05
-        phi_limit = 0.7*np.pi/2
+        psi_err = np.mod((wp_dir - psi) + np.pi , 2*np.pi) - np.pi
 
-        self.psi_integrator += psi_err*self.alt_control_period
-        if K_i_head > 0:
-            self.psi_integrator = saturate(self.psi_integrator, phi_limit/K_i_head)
+        if d > 15:
+            curv = 2 * psi_err / d
+            curv *= 0.8 # sideslip correction
+        else:
+            curv = self.last_curv
+            
+        self.last_curv = curv
+        g = 9.81
+        #K_p_head = 0.8
+        phi_limit = np.arccos (18 * g / self.airSpeed**2)
 
-        d_psi_err = (psi_err - self.last_psi_err) / self.alt_control_period
-        self.last_psi_err = psi_err
+        #psi_dot_ref = K_p_head * psi_err 
+        psi_dot_ref = self.airSpeed * curv
+        
+        phi_ref = - np.arctan2(psi_dot_ref * self.airSpeed, g)
 
-        if self.last_wp_id != wp_id:
-            d_psi_err = 0
-            self.psi_integrator = 0
+        if np.abs(phi_ref) >= phi_limit:
+            print("Banking at the limit")
 
-        phi_ref = -( K_p_head * psi_err + K_d_head * d_psi_err + K_i_head * self.psi_integrator)
         phi_ref = saturate(phi_ref,  phi_limit)
+
+
 
         self.phi_ref = phi_ref
 
-        wp_dist = np.linalg.norm([wp_x-x, wp_y-y])
         radius = np.sqrt(x**2 + y**2)
         
-        print('z: {:.1f}\trho: {:.1f}\twp: {:.1f}°\td: {:.1f}\tps_err: {:.1f}\tph_ref: {:.1f}\tth_ref: {:.1f}\t'.format(alt, radius, wp_ang_position*180/np.pi, wp_dist, psi_err, self.phi_ref, self.theta_ref))
+        print('v: {:.1f}\tpr: {:.1f}\tz: {:.1f}\trho: {:.1f}\td: {:.1f}\tps_err: {:.1f}\tph_err: {:.1f}\tth_ref: {:.1f}\tth: {:.1f}\t'\
+              .format(self.airSpeed,self.prop, alt, radius, d, psi_err, self.last_phi_err, self.theta_ref, self.theta))
         #print('z: {:.1f}\tph_ref: {:.1f}\tth_ref: {:.1f}\t'.format(alt, self.phi_ref, self.theta_ref))
         #print('elev: {:.2f}\tth: {:.2f}\tth_ref: {:.2f}\talt: {:.2f}'.format(self.elev, self.theta, self.theta_ref, alt))
 
@@ -143,46 +151,52 @@ class Learn2Soar:
             
         ## Roll controller
 
-        K_p_r = 200
-        ph_lim = 0.8*np.pi/2
+        K_p_r = 600
+        K_d_r = 250
+        ail_lim = 0.5*np.pi/2
 
         phi_ref = self.phi_ref
         phi     = euler[0]
         phi_err = phi_ref - phi
 
-        ail_pos = 1 / airSpeed**2 * K_p_r * phi_err
-        ail_pos = saturate(ail_pos, ph_lim)
+        d_phi_err = (phi_err - self.last_phi_err ) / self.roll_pitch_control_period
+        self.last_phi_err = phi_err
+
+        ail_pos = 1 / airSpeed**2 * (K_p_r * phi_err + K_d_r * d_phi_err)
+        ail_pos = saturate(ail_pos, ail_lim)
 
         ## Pitch controller 
 
         K_p_p = 250
         elev_trim = -0.05
-        th_lim = 1
+        elev_lim = 0.3*np.pi/2
 
         theta_ref = self.theta_ref 
         theta     = euler[1]
         theta_err = theta_ref - theta
 
-        if np.abs( np.abs(phi) - np.pi/2 ) < 0.01:
-            elev_pos = elev_trim  # can't control pitch with elevator when sideways
-            print("SIDEWAYS!!")
+        if np.pi/2 - np.abs(phi) < 0.001:
+            elev_pos = 0 
         else:
-            elev_pos = 1 / np.cos(phi) * ( 1 / airSpeed**2 * K_p_p * theta_err + elev_trim )
+            elev_pos = 1/np.cos(phi) * ( 1/airSpeed**2 * K_p_p * theta_err + elev_trim )
 
-        elev_pos = saturate(elev_pos, th_lim)
+        elev_pos = saturate(elev_pos, elev_lim)
 
 
-        ## Speed controller
-
-        prop = 10000
+        ## Speed controller, very rough, upsgrade to PI if you care
+        k_p_v = 5000
+        v_ref = 12
+        prop_ff = 900
+        self.prop = prop_ff + k_p_v * (v_ref - airSpeed)
+        self.prop = max(0, min(10000, self.prop))
 
 
         ## Useful values to access throughout in the class 
         self.theta = theta
         self.phi = phi
+        self.airSpeed = airSpeed
         self.ail  = ail_pos
         self.elev = elev_pos
-        self.prop = prop
 
         # command = [
         #   rudd_pos  
@@ -196,6 +210,6 @@ class Learn2Soar:
         #   prop_ref_0
         # ]
 
-        command = [0, elev_pos, ail_pos, -ail_pos, 0, 0 , 0, 0, prop]
+        command = [0, elev_pos, ail_pos, -ail_pos, 0, 0 , 0, 0, self.prop]
 
         return command
