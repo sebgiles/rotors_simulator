0c0eae303b092e1d1538875c8fa0c5e347164adf
seb <sgiles@student.ethz.ch>
2020-05-14 23:18:51 +0200

    Give feedback on terminal during mouse teleop

--- learn2soar/scripts/manual_albatross.py
+++ learn2soar/scripts/manual_albatross.py
@@ -7,12 +7,12 @@ from teleop.msg import TwoTuple
 from std_msgs.msg import Float32, Bool
 
 import gym
-import rotors_gym_envs.learn_to_albatross_env_v0
+import rotors_gym_envs.l2s_energy_env_v1
 import time
 class HumanInTheLoop():
     
     def __init__(self):
-        self.action = [0.0, 0.0]
+        self.mouse_cmd = [0.0, 0.0]
         self.paused = True
         rospy.Subscriber("/teleop_mouse_cmd", TwoTuple,
                          self._mouse_cb, queue_size=1)
@@ -20,12 +20,14 @@ class HumanInTheLoop():
                          self._click_cb, queue_size=1)
     
 
-    def get_action(self):
+    def get_action(self, obs):
         while self.paused:
             if rospy.is_shutdown(): 
                 raise KeyboardInterrupt
             time.sleep(0.1)
-        return self.action
+        pitch, roll = self.mouse_cmd
+        action = [pitch, roll]
+        return action
             
 
     def _click_cb(self, msg):
@@ -33,21 +35,28 @@ class HumanInTheLoop():
 
 
     def _mouse_cb(self, msg):
-        self.action = [msg.v, -msg.h]
+        self.mouse_cmd = [msg.v, -msg.h]
 
 
 def main():
-    env = gym.make('Albatross-v0')
+    env = gym.make('l2s-energy-v1')
     agent = HumanInTheLoop()
     done = True
-    while True:
-        if done or agent.paused: env.reset()
+    ep_rew = 0
+    while True:  
+        if done or agent.paused: 
+            print(ep_rew)
+            obs = env.reset()
+            ep_rew = 0
         try:
-            act = agent.get_action()
+            act = agent.get_action(obs)
         except KeyboardInterrupt:
             print()
             exit()
-        _, _, done, _ = env.step(act)
+        obs, rew, done, _ = env.step(act)
+        print(obs)
+        ep_rew += rew
+        
 
 
 if __name__ == '__main__':
